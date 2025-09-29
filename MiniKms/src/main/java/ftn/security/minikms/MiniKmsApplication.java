24875851b79284ddfc977ff2b4eb58bb31932074
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2025-09-29 19:42:39 +0200

    Initialize springboot backend

---
+++ MiniKms/src/main/java/ftn/security/minikms/MiniKmsApplication.java
@@ -0,0 +1,13 @@
+package ftn.security.minikms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniKmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniKmsApplication.class, args);
+    }
+
+}
