3594e9c65f340cf7e1b6c54cb92298a16eb44b4d
Masa Ivanov <masa.ivanov04.10@gmail.com>
2025-10-01 18:01:38 +0200

    Add manage keys looks

--- MiniKmsClient/src/app/manage-keys/manage-keys.component.ts
+++ MiniKmsClient/src/app/manage-keys/manage-keys.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface KeyRecord {
+  keyId: string;
+  alias: string;
+  keyType: string;
+}
 
 @Component({
   selector: 'app-manage-keys',
@@ -6,5 +13,28 @@ import { Component } from '@angular/core';
   styleUrl: './manage-keys.component.scss'
 })
 export class ManageKeysComponent {
+keyTypes: string[] = ['AES', 'RSA', 'HMAC'];
 
+  displayedColumns: string[] = ['keyId', 'alias', 'keyType', 'actions'];
+  dataSource = new MatTableDataSource<KeyRecord>([
+    { keyId: '1', alias: 'Key One', keyType: 'AES' },
+    { keyId: '2', alias: 'Key Two', keyType: 'RSA' },
+    { keyId: '3', alias: 'Key Three', keyType: 'AES' },
+    { keyId: '4', alias: 'Key Four', keyType: 'HMAC' }
+  ]);
+
+  addKey() {
+    console.log('Add key clicked');
+    // TODO: implement
+  }
+
+  deleteKey(key: KeyRecord) {
+    console.log('Delete key', key);
+    // TODO: implement
+  }
+
+  rotateKey(key: KeyRecord) {
+    console.log('Rotate key', key);
+    // TODO: implement
+  }
 }
