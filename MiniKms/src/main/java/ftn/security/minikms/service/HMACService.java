309ab259b1513396cd3721a70899f958a2b4cc70
Masa Ivanov <masa.ivanov04.10@gmail.com>
2025-10-01 11:22:02 +0200

    Add compute and verify Hmac

--- MiniKms/src/main/java/ftn/security/minikms/service/HMACService.java
+++ MiniKms/src/main/java/ftn/security/minikms/service/HMACService.java
@@ -3,8 +3,13 @@ package ftn.security.minikms.service;
 import ftn.security.minikms.entity.KeyMaterial;
 
 import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Base64;
 
 class HMACService implements ICryptoService {
     public KeyMaterial generateKey() throws NoSuchAlgorithmException {
@@ -12,5 +17,16 @@ class HMACService implements ICryptoService {
         keyGenerator.init(256, SecureRandom.getInstanceStrong());
         var key = keyGenerator.generateKey();
         return KeyMaterial.of(key);
+    }
+    public static String computeHmac(String message, KeyMaterial key) throws Exception {
+        Mac mac = Mac.getInstance("HmacSHA512");
+        mac.init(new SecretKeySpec(key.getKey(),"HmacSHA512"));
+        byte[] hmacBytes = mac.doFinal(message.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(hmacBytes);
+    }
+    public static boolean verifyHmac(String message, String hmacBase64, KeyMaterial key) throws Exception {
+        String computedHmac = computeHmac(message, key);
+        return MessageDigest.isEqual(computedHmac.getBytes(StandardCharsets.UTF_8),
+                hmacBase64.getBytes(StandardCharsets.UTF_8));
     }
 }
